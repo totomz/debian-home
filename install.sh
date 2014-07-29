a2e8e5982da4e4c458f305f96c263d26f5e715b0
Tommaso Doninelli <tommaso.doninelli@pc-doninelli.ad.sistemaits.com>
2014-07-29 10:22:39 +0200

    Xbindkeys now is optional

--- install.sh
+++ install.sh
@@ -8,7 +8,13 @@ find -maxdepth 1 -mindepth 1 -not -name .git -not -name README.md -not -name LIC
 source ~/.bashrc
 
 # Download Xbindkeys (to control Spotify)
-sudo apt-get install xbindkeys
-sudo apt-get install xbindkeys-config
-sudo apt-get install xvkbd
-xbindkeys
+echo "Install xbindkeys? (y|N)"
+read doInst
+if [ "$doInst" == 'y' ] 
+then
+    sudo apt-get install xbindkeys
+    sudo apt-get install xbindkeys-config
+    sudo apt-get install xvkbd
+    xbindkeys
+fi
+
